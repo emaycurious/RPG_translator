bd66ed675c2b5929ea54a19bb02c531b0a4a172a
emay <164755861+emaycurious@users.noreply.github.com>
2025-10-07 20:16:05 +0800

    Add files via upload

---
+++ screen/screen_001.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+# root.wm_attributes('-alpha', 0.1)  # 设置窗体透明度为 50%
+
+root.attributes('-type', 'splash')  # 或 'splash'
+root.attributes('-alpha', 0.5)
+
+root.mainloop()
